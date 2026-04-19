fdd80afb4101e4e8188dcad324846ca738021490
379210 <evan.gallardo.laces@gmail.com>
2026-04-19 17:15:14 +0000

    hello.ts

---
+++ ts-projects/src/01-hello.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const myName = "Nicolas";
+console.log(myName);
+//# sourceMappingURL=01-hello.js.map
--- ts-projects/src/demo.js
+++ ts-projects/src/demo.js
@@ -1,39 +1,38 @@
-// @ts-check
-
-(async ()=> {
-  const myCart = [];
-  const products = [];
-  const limit = 2;
-
-  async function getProducts() {
-    const rta = await fetch('http://api.escuelajs.co/api/v1/products', {
-      method: 'GET'
-    });
-    const data = await rta.json();
-    products.concat(data);
-  }
-  function getTotal() {
-    const total = 0;
-    for (let i = 0; i < products.length; i++) {
-      total += products[i].prize;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+(async () => {
+    const myCart = [];
+    const products = [];
+    const limit = 2;
+    async function getProducts() {
+        const rta = await fetch('http://api.escuelajs.co/api/v1/products', {
+            method: 'GET'
+        });
+        const data = await rta.json();
+        products.concat(data);
     }
-    return total;
-  }
-  function addProduct(index) {
-    if (getTotal <= limit) {
-      myCart.push(products[index]);
+    function getTotal() {
+        let total = 0;
+        for (let i = 0; i < products.length; i++) {
+            total += products[i].prize;
+        }
+        return total;
     }
-  }
-
-  await getProducts();
-  addProducto(1);
-  addProducto(2);
-  const total = getTotal();
-  console.log(total);
-  const person = {
-    name: 'Nicolas',
-    lastName: 'Molina'
-  }
-  const rta = person +  limit;
-  console.log(rta);
+    function addProduct(index) {
+        if (getTotal() <= limit) {
+            myCart.push(products[index]);
+        }
+    }
+    await getProducts();
+    addProduct(1);
+    addProduct(2);
+    const total = getTotal();
+    console.log(total);
+    const person = {
+        name: 'Nicolas',
+        lastName: 'Molina'
+    };
+    // const rta = person +  limit;
+    // console.log(rta);
 });
+//# sourceMappingURL=demo.js.map
