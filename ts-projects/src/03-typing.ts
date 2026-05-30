e3a281f01cff1aea637d1949ddbc26820254f63c
379210 <evan.gallardo.laces@gmail.com>
2026-05-30 03:42:24 +0000

    03-typing.ts

--- ts-projects/src/03-typing.ts
+++ ts-projects/src/03-typing.ts
@@ -1,1 +1,13 @@
-let myProductNAme = 'Product 1';
+(() => {
+  let myProductName = 'Product 1';
+  let myProductPrice = 123;
+
+  let myProductName = 'change';
+  myProductName.toLowerCase();
+
+  myProductPrice.toFixed();
+
+  const myProductStock = 1000;
+
+  const myProductName2 = 'Product 1';
+})();
